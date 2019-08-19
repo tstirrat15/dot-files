9e7b97d11515662052cff4986baee1d917a86995
Tanner Stirrat <tstirrat@gmail.com>
2019-08-19 15:31:12 -0600

    Remove reference to JS file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -140,7 +140,6 @@
 
 ;; Langauage-specific
 (load "setup-clojure.el")
-(load "setup-js.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
